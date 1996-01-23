15156cf3ddfe2425257b068cb29758a201690e01
Rik Faith <rikfaith@gmail.com>
1996-01-23 03:21:10 +0000

    Make top-level make friendlier

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Mon Jan 22 22:01:18 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.4 $";
+/* Stamp: Mon Jan 22 22:15:00 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.5 $";
