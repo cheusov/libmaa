acf2a0336e5781307a6636c48c0c9efe8960aa51
Rik Faith <rikfaith@gmail.com>
1996-01-23 03:07:28 +0000

    Fix distclean rule

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Mon Jan 22 19:23:32 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.3 $";
+/* Stamp: Mon Jan 22 22:01:18 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.4 $";
