6414c7e2ed542f1f13a8c0cc78220c895355b3c8
Rik Faith <rikfaith@gmail.com>
1996-01-23 02:51:05 +0000

    Make new tree hierarchy configure and compile

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Sat Jan 20 11:37:28 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.2 $";
+/* Stamp: Mon Jan 22 19:23:32 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.3 $";
