f4a961088c03bb4cd867c22869c99e452eb72626
Rik Faith <rikfaith@gmail.com>
1996-01-27 15:41:46 +0000

    unhose version.c

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Thu Jan 25 16:17:33 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.6 $";
+/* Stamp: Sat Jan 27 10:41:00 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.7 $";
