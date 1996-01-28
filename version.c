5f790edad3aa5638b84ccc1b4774efdc3d7c76d2
Rik Faith <rikfaith@gmail.com>
1996-01-28 00:35:38 +0000

    Fixed latest parsing problem Added support for pre-processor line number directives Trying to get interesting symbols printed better -- need to tag all of the treevar variables as special, so they will not be printed every time.\ Need to add support for forest replacements to Khepera language.

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Sat Jan 27 10:41:00 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.7 $";
+/* Stamp: Sat Jan 27 10:42:08 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.8 $";
