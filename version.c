f138e2b374f6afc84c99e173e38caff8a66859dd
Rik Faith <rikfaith@gmail.com>
1996-01-28 19:29:33 +0000

    Fixed forest support Iterator expansion rules that we added Friday now work.

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Sat Jan 27 20:29:17 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.9 $";
+/* Stamp: Sun Jan 28 08:53:43 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.10 $";
