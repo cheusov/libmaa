e7146cd0e618ca5aa2e44c30ae9c6204c4f3e7cb
Rik Faith <rikfaith@gmail.com>
1996-01-28 13:39:31 +0000

    Added forest support to Khepera language

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Sat Jan 27 10:42:08 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.8 $";
+/* Stamp: Sat Jan 27 20:29:17 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.9 $";
