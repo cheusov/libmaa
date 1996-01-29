828c25640fbbcde6b6c16ae7ffd51371fbb65151
nyland <nyland>
1996-01-29 16:35:03 +0000

    Added some type inference rules.

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Sun Jan 28 21:13:38 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.12 $";
+/* Stamp: Mon Jan 29 10:56:34 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.13 $";
