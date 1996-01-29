f1f1fe1b347876589be02834cbd8e0cd263f0831
Rik Faith <rikfaith@gmail.com>
1996-01-29 14:28:38 +0000

    Made most nodes constant arity -- the type-declaration nodes can probably be of variable arity without problems.

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Sun Jan 28 14:42:28 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.11 $";
+/* Stamp: Sun Jan 28 21:13:38 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.12 $";
