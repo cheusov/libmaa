db7d8fba71f4e3f260771775a5364cef7aba1d40
Rik Faith <rikfaith@gmail.com>
1996-02-04 04:11:59 +0000

    Added stubs so that work on flattening transforms can progress. Made all *.k files compile directly to a *.o file, instead of being included in some other file.  This is much better.

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Wed Jan 31 09:27:51 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.16 $";
+/* Stamp: Sat Feb  3 21:22:05 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.17 $";
