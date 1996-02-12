a8f244332e927735398d3575eed521982eb15f83
Rik Faith <rikfaith@gmail.com>
1996-02-12 00:49:08 +0000

    Toward red-black tree.  I am going to add parent node and use the algorithms from Cormen, et al. Until then, use the -d.binary switch for unbalanced trees.

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Sun Feb 11 09:56:13 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.20 $";
+/* Stamp: Sun Feb 11 18:35:18 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.21 $";
