ace68729672380d89b1cb6ded5628eed398be98b
Rik Faith <rikfaith@gmail.com>
1996-02-24 16:02:04 +0000

    Bump version.

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Fri Feb 23 16:18:51 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.24 $";
+/* Stamp: Sat Feb 24 07:56:46 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.25 $";
