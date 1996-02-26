5f7afaee79b3298361e242f2ca68ac33c272a9e5
Rik Faith <rikfaith@gmail.com>
1996-02-26 19:57:17 +0000

    For MIPSEL/DECstation port

--- strdup.c
+++ strdup.c
@@ -1,13 +1,13 @@
 /* strdup.c -- String duplicate function
  * Created: Mon Nov  7 10:23:32 1994 by faith@cs.unc.edu
- * Revised: Sun Nov 19 13:30:19 1995 by faith@cs.unc.edu
- * Public Domain 1994, 1995 Rickard E. Faith (faith@cs.unc.edu)
+ * Revised: Mon Feb 26 11:18:58 1996 by faith@cs.unc.edu
+ * Public Domain 1994, 1995, 1996 Rickard E. Faith (faith@cs.unc.edu)
  *
  * This function is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * 
- * $Id: strdup.c,v 1.2 1995/11/19 19:28:50 faith Exp $ */
+ * $Id: strdup.c,v 1.3 1996/02/26 19:57:17 faith Exp $ */
 
 #include "maaP.h"
 
@@ -17,7 +17,7 @@
 void *malloc();
 #endif
 
-char *strdup( char *s)
+char *strdup( const char *s)
 {
    int   len = strlen( s );
    char *r;
