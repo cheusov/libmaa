bcd98e9c797349bbabb5c77021ea3469ce7a8a41
Rik Faith <rikfaith@gmail.com>
1996-03-03 23:59:09 +0000

    Added rebuild, if, break, return to Khepera language.  I need to think about a way to detect and/or mark builds that result in memory leaks...

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Tue Feb 27 13:24:01 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.32 $";
+/* Stamp: Sun Mar  3 17:19:45 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.33 $";
