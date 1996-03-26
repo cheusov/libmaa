9d337fe40a6bd87ac9ba73889a428f1a9aecda33
Bill Yakowenko <yakowenk@cs.unc.edu>
1996-03-26 17:10:01 +0000

    increment version number

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Fri Mar 22 13:46:15 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.37 $";
+/* Stamp: Mon Mar 25 23:05:34 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.38 $";
