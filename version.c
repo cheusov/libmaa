76c19c8d078a59e68c51d9be89027a46bda7c435
Rik Faith <rikfaith@gmail.com>
1996-04-03 20:49:52 +0000

    Merged rik-2 with main trunk; Fixed tracking bug.

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Wed Mar 27 16:56:58 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.40 $";
+/* Stamp: Wed Apr  3 15:15:06 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.41 $";
