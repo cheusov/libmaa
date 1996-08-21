edf437c43129b934e516a64d7bd012c5fadc8f42
Rik Faith <rikfaith@gmail.com>
1996-08-21 02:50:07 +0000

    Fortran types

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Sat Aug 17 09:38:33 EDT 1996 */
-const char *maa_revision_string = "$Revision: 1.46 $";
+/* Stamp: Mon Aug 19 11:01:14 EDT 1996 */
+const char *maa_revision_string = "$Revision: 1.47 $";
