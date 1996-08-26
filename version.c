f1385bba0fe77a7fbbdcc00a7118b73c7e4d6727
Rik Faith <rikfaith@gmail.com>
1996-08-26 16:05:30 +0000

    Work on Fortran -- checkin to take home

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Mon Aug 19 11:01:14 EDT 1996 */
-const char *maa_revision_string = "$Revision: 1.47 $";
+/* Stamp: Fri Aug 23 09:40:31 EDT 1996 */
+const char *maa_revision_string = "$Revision: 1.48 $";
