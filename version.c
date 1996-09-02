3e66a6cfa47dd6f10a5449f6fd42c0f1773fca63
Rik Faith <rikfaith@gmail.com>
1996-09-02 01:46:45 +0000

    Random fixes

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Fri Aug 23 09:40:31 EDT 1996 */
-const char *maa_revision_string = "$Revision: 1.48 $";
+/* Stamp: Mon Aug 26 13:27:42 EDT 1996 */
+const char *maa_revision_string = "$Revision: 1.49 $";
