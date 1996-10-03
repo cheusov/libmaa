b5523d4880fe002720ba3f820826b4b1736ce3fd
Rik Faith <rikfaith@gmail.com>
1996-10-03 01:58:27 +0000

    Added -with-nox option to inhibit building X applications Added prtest to libmaa suite

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Wed Oct  2 14:25:04 EDT 1996 */
-const char *maa_revision_string = "$Revision: 1.58 $";
+/* Stamp: Wed Oct  2 21:50:00 EDT 1996 */
+const char *maa_revision_string = "$Revision: 1.59 $";
