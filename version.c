80e8f92b8fdc9626acb18ea5e98add5831b8a334
Rik Faith <rikfaith@gmail.com>
1996-10-31 21:25:45 +0000

    Added dpl_build_* Added ability to use .. (sametypes) wildcard in var declaration   (this is essential to be able to type dpl_build_*)

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Tue Oct 29 15:14:37 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.61 $";
+/* Stamp: Thu Oct 31 09:49:45 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.62 $";
