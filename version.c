8285e1600246f3be2f682e5f94764f1ed06c1233
Rik Faith <rikfaith@gmail.com>
1996-11-11 01:54:37 +0000

    1) Added code to take advantage of 1 and 2 parameter versions of dpl_range* 2) Added transformations for dpl_dist*, dpl_restrict* 3) Print warning when an AST node does not have an entry in header.pro    (a type isn't needed, just an entry -- at least this way we know about    it; and, because of the powerful type inference engine, everything    doesn't have to be explicitly typed -- the engine will figure out what    the type has to be most (all?) of the time). todo) work on -fflatten and -fsimple, especially for if statements

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Sun Nov 10 09:54:35 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.67 $";
+/* Stamp: Sun Nov 10 15:23:15 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.68 $";
