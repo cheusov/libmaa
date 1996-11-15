c9773aff447a1e4f6e33d705c1aa1ba48bcae757
Rik Faith <rikfaith@gmail.com>
1996-11-15 22:40:12 +0000

    New example

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Mon Nov 11 12:05:31 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.69 $";
+/* Stamp: Fri Nov 15 11:13:34 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.70 $";
