e341648a50e1ab07d81abaee3ab96ea681585563
Rik Faith <rikfaith@gmail.com>
1996-11-19 22:34:27 +0000

    1) Finally got Palmer96 rules 1-5 implemented.  There are a few problems:     a) Primed function duplication doesn't work unless the type of the        function is either specified or can be derived.  It can't be derived        if the prime function isn't used.  The right way to do this is to        delete primed functions (or not generated them) unless they are        used.  However, due to POLYMORPHISM, this isn't possible without        cooperation from the type system.  Adding this cooperation will take        time.  Hence, I'm punting on this until we deal with polymorphism in        general.     b) I used the "James" method of generating primed functions, and I        haven't written the part that pushed the zip'd components through to        indexing.  I'll do that next. 2) Removed Lar's optimiztion.k for the time being.  It doesn't help us to    actually generate code and only confuses things.  I'll put it back after    we can generate C+DPL. 3) Redid all the call/uncall stuff using a header file.  Now, one line has    to be added and the call/uncall functions will get created _and_ mapped    into the xfm Rule body.  See functions.h for the list. 4) Deactivated a bunch of transformations that do not get us any closer to    executable code. 5) Commented some of the rules. 6) Fixed a few aesthetic bugs in error printing. 7) Saved Lar's transform.c as transform.lars.c. 8) Tried to fix the enclosed version of cpp to implement the ANSI #    stringification directive.  Gave up.  Patched the parser routines to    find the native cpp instead.  Will delete the local cpp next time -- its    just too old to be useful, and ptrans requires an ANSI C compiler    anyway. 9) tre_fast_postorder_allowed lets you know if a tre_fast_postorder walk is    possible.  It isn't right now if you aren't walking from the root of the    labeled tree.

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Fri Nov 15 11:13:34 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.70 $";
+/* Stamp: Sun Nov 17 17:39:38 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.71 $";
