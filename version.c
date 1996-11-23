6cc0ade64f6610626f8d50182f772a6611776ce6
Rik Faith <rikfaith@gmail.com>
1996-11-23 23:58:11 +0000

    1) Make add_primed_func_defns propagate the return type as well as the    argument types. 2) Added functions to push [a in a_p, b in b_p, ... : ... ] through the    expression as index operations (alternatively, this could be implemented    with the now-non-existent zip primitive). 3) Added functions to handle simple dom() simplification.  More complex    functions will be easy to add as needed. 4) Convert (N_Sequence (N_ExpressionList ...)) into (N_Build ...) so that    these node can be interconverted with N_Call nodes, allowing iterators    to be pushed through them. 5) Added support for the "walk" keyword in the Khepera language.  This    allows a pre-order walk to be performed of an arbitrary node, much like    the "children" keyword allows the children to be iterated over. 6) Fixed bug where deleting nodes which were queued for deletion caused a    non-reentrant function to be called twice.  (Made tre_delete_queued    reentrant.) 7) Added font-lock mode definition as a comment in khepera-mode.el.
    This all means that Dan's Rule 0 through Rule 5 are fully impemented.  I'm
    going to work on function call parameter lifting next, and then on last-use
    detection so that DPL memory management can be added....

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Sun Nov 17 17:39:38 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.71 $";
+/* Stamp: Sat Nov 23 11:39:10 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.72 $";
