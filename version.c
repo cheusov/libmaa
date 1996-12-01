1ba5b37b5adcf82efba48f3b7111e7beb2e8920a
Rik Faith <rikfaith@gmail.com>
1996-12-01 23:44:25 +0000

    Added query and if-then-else simplification.

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Sat Nov 30 17:15:33 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.74 $";
+/* Stamp: Sun Dec  1 11:09:50 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.75 $";
