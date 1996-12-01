ad88aa23b0b8fee19dca6bdf87648d2cdbc049a1
Rik Faith <rikfaith@gmail.com>
1996-12-01 02:30:21 +0000

    When looking for cpp, first try to use the one that comes with gcc.  Then look on the system for one.  We should probably incorporate the GNU cpp and hack it so that it won't print warnings for Proteus code.  For now, just redirect errors to /dev/null.

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Mon Nov 25 08:57:19 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.73 $";
+/* Stamp: Sat Nov 30 17:15:33 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.74 $";
