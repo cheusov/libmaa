8316c488766fa5300ef7dd2b50193207063c97f8
Rik Faith <rikfaith@gmail.com>
1996-12-14 21:33:45 +0000

    1) Changed spacing of ChangeLog 2) Re-wrote let flattening (Palmer96 Rule1).  But still need to fixup    scope. 3) Re-wrote zip flattening (Palmer96 Rule5).  Would like to simplify this    by adding a Khepera "optional" match. 4) Added much more readable output for -V tracing (-dverbose). 5) Added sscope fixups to be automatically applied at each insertion. 6) Added printing support for unique node values (-d.unique). 7) Experimented with removing symbols from the symbol table.  This is    probably a very bad idea, but I'll leave the code there for now.  (The    functions aren't used.) 8) Made removal of pending nodes and queuing of pending nodes a critical    section. 9) Made fast post-order walks actually work.  (The logic had been inverted,    so they were incorrectly used on small trees, but not on the main tree.)
    Still working on let scoping debugging.

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Sun Dec  8 11:56:07 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.77 $";
+/* Stamp: Sat Dec 14 13:53:48 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.78 $";
