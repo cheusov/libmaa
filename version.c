ce13845fe6b1a401365d236886734cbc9d507128
Rik Faith <rikfaith@gmail.com>
1996-12-23 03:09:06 +0000

    1) Get insert/extract working.  For type checking, there is now a callback    that will be called whenever a type definition cannot be found in the    usual ways (this is a ``last resort'' callback).  The extract/insert    type creation callback examines the current tree and creates an    inference rule for it, making an entry in the symbol table so that this    won't have to be regenerated again (although this may not currently be    in the proper scope).  Next, I'll use this same callback to generate    inference rules for indexing instances. 2) Re-did all the range stuff.  Now N_Range1, N_Range2, and N_Range3 are    carried through the transformations end-to-end.  This allows early    detection of range1, and prevents a lot of dists of 1's.  It also means    that we can avoid dpl_st_range_p at the Proteus-source level, since this    function is not working in DPL. 3) Added transforms to DPL calls for #, +/, ++, and a few others (?). 4) Fixed some places where _tre_DeleteQueueProhibited was not being    properly restored.  This led to a situation where the delete queue was    never emptied, which led to infinite attempts to delete the deleted    node.
    Next, figure out how to type check indexing.  Was the type system designed
    to handle this?

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Sat Dec 21 08:39:52 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.79 $";
+/* Stamp: Sun Dec 22 04:03:47 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.80 $";
