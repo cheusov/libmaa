331ab54b5718c46b3571172128effc769b488f42
Rik Faith <rikfaith@gmail.com>
1996-12-25 06:14:44 +0000

    Major milestone: qsort runs to completion after a little tweaking. Here's what needs fixing:     nseq assignment needs to use dpl_copy     dpl_st_range uses the old style ranges, not the ones used by N_Range
    Other stuff in this checking:
    1) Exapanded ex4.pro so debug indexing
    2) Finished implementation of flat indexing (vs. SelectSeq)
    3) Added new functions: leseq, combine, flatten, concat
    4) Convert dom into range1
    5) Output DPL for index, concatreduce, combine
    6) Added ad hoc on-the-fly type generation for indexing (similar to that
       for insert/extract)
    7) Fix insert/extract on-the-fly type generation
    8) Print out types of all assignments -- this helps with debugging
    9) Fix type printing as called by tre_print.  Now type variables in a
       tre_print will be correctly identified as 'a, 'b, etc.  Before they
       might all be called 'a even when they were different.  (This was an
       error of printing -- the types were correctly stored as differentiated
       variables.)

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Sun Dec 22 04:03:47 EST 1996 */
-const char *maa_revision_string = "$Revision: 1.80 $";
+/* Stamp: Tue Dec 24 10:53:59 EST 1996 */
+const char *maa_revision_string = "$Revision: 1.81 $";
