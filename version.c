0f551af56d5a98bf30fb1f1f6736b983eee94973
Rik Faith <rikfaith@gmail.com>
1997-03-01 04:48:31 +0000

    Example ocnfig file

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Fri Feb 28 09:43:14 EST 1997 */
-const char *maa_revision_string = "$Revision: 1.87 $";
+/* Stamp: Fri Feb 28 23:26:11 EST 1997 */
+const char *maa_revision_string = "$Revision: 1.88 $";
