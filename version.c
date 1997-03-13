498c8ccadf038170b6951e6ff753d5737bec7626
Rik Faith <rikfaith@gmail.com>
1997-03-13 05:27:43 +0000

    Added -a option to select fast labeling algorithms Implemented Algorithm 0, 1, 2, noting speed improvement I plan to break Algorithm 3 into 3 and 4 (and 5?), since it is a big step up from Algorithm 2, and may not be any faster...

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
-/* Stamp: Mon Mar 10 23:36:17 EST 1997 */
-const char *maa_revision_string = "$Revision: 1.93 $";
+/* Stamp: Wed Mar 12 09:12:54 EST 1997 */
+const char *maa_revision_string = "$Revision: 1.94 $";
