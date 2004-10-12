5db4e6a105ead44f7c78a71e73a81ee079fc9228
Aleksey Cheusov <vle@gmx.net>
2004-10-12 10:23:38 +0000

    debugging information switched on using -d option of dictd     is logged to syslog/file/FILE_stream, but printf'ed to stdout.

--- maa.h
+++ maa.h
@@ -17,7 +17,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  * 
- * $Id: maa.h,v 1.41 2003/12/30 16:28:20 cheusov Exp $
+ * $Id: maa.h,v 1.42 2004/10/12 10:23:38 cheusov Exp $
  */
 
 #ifndef _MAA_H_
@@ -437,8 +437,7 @@ extern void           dbg_unset_flag( dbg_Type flag );
 extern __inline__ int dbg_test( dbg_Type flag );
 extern void           dbg_list( FILE *stream );
 
-#define PRINTF(flag,arg)  if (dbg_test(flag)) { printf arg; }
-#define FPRINTF(flag,arg) if (dbg_test(flag)) { fprintf arg; }
+#define PRINTF(flag,arg)  if (dbg_test(flag)) { log_info arg; }
 
 /* flags.c */
 
