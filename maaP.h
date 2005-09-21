5e1506420001401c92a9b6935ed362ecc794c9ba
Aleksey Cheusov <vle@gmx.net>
2005-09-21 19:04:49 +0000

    fixed: compilation bug (alloca)

--- maaP.h
+++ maaP.h
@@ -17,7 +17,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  * 
- * $Id: maaP.h,v 1.23 2005/09/05 18:00:02 cheusov Exp $
+ * $Id: maaP.h,v 1.24 2005/09/21 19:04:49 cheusov Exp $
  */
 
 #ifndef _MAAP_H_
@@ -33,11 +33,12 @@
 #define __FUNCTION__ __FILE__
 #endif
 
-/* AIX requires this to be the first thing in the file.  */
-#ifndef HAVE_ALLOCA
 #if HAVE_ALLOCA_H
 # include <alloca.h>
-#else
+#endif
+
+/* AIX requires this to be the first thing in the file.  */
+#ifdef HAVE_ALLOCA
 # ifdef _AIX
 #  pragma alloca
 # else
@@ -45,7 +46,6 @@
     char *alloca ();
 #  endif
 # endif
-#endif
 #endif
 
 /* Get string functions */
