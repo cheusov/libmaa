8eb9fff53958df49f52671b3a879165152e5591e
Aleksey Cheusov <vle@gmx.net>
2020-03-17 22:58:34 +0300

    tests/err: add more tests for err_* functions

--- tests/err/errtest.c
+++ tests/err/errtest.c
@@ -15,7 +15,30 @@ int main(int argc, char **argv)
 
    switch (num){
    case 1:
-      err_fatal(__func__, "qqq\n");
+      err_fatal(__func__, "fatal error #%d\n", 56);
+      break;
+   case 2:
+      err_fatal(NULL, "fatal error #%d\n", 711);
+      break;
+   case 3:
+      err_set_program_name(NULL);
+      err_fatal(NULL, "fatal error #%d\n", 75);
+      break;
+   case 4:
+      read(100, NULL, 0);
+      err_fatal_errno(__func__, "fatal error #%d\n", 17);
+      break;
+   case 5:
+      err_warning(__func__, "warning #%d\n", 11);
+      break;
+   case 6:
+      err_warning(NULL, "warning #%d\n", 11);
+      break;
+   case 7:
+      err_internal(NULL, "internal error #%d\n", 71);
+      break;
+   case 8:
+      err_internal(NULL, "internal error #%d\n", 71);
       break;
    default:
       fprintf(stderr, "incorrect argument '%s' was specified\n", argv[1]);
--- tests/err/test.mk
+++ tests/err/test.mk
@@ -1,8 +1,14 @@
 .PHONY : test_output
 test_output:
 	@LD_LIBRARY_PATH=${OBJDIR_maa}; export LD_LIBRARY_PATH; \
-	${.OBJDIR}/errtest 1 2>&1; \
-	echo '$$?='$$?; \
+	${.OBJDIR}/errtest 1 2>&1; echo '$$?='$$?; \
+	${.OBJDIR}/errtest 2 2>&1; echo '$$?='$$?; \
+	${.OBJDIR}/errtest 3 2>&1; echo '$$?='$$?; \
+	${.OBJDIR}/errtest 4 2>&1 | sed 's/^main:.*$$/main: Bad file descriptor/'; \
+	${.OBJDIR}/errtest 5 2>&1; echo '$$?='$$?; \
+	${.OBJDIR}/errtest 6 2>&1; echo '$$?='$$?; \
+	ulimit -c 0; ${.OBJDIR}/errtest 7 2>&1; echo '$$?='$$?; \
+	ulimit -c 0; ${.OBJDIR}/errtest 8 2>&1; echo '$$?='$$?; \
 	true
 
 .include <mkc.minitest.mk>
