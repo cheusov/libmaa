624a5b5f5e7a47ce85c041db84cb41740ba64903
Aleksey Cheusov <vle@gmx.net>
2020-03-29 13:51:00 +0200

    Fix tests/err test on non-NetBSD/non-Linux systems (different strerror(errno))

--- tests/err/test.mk
+++ tests/err/test.mk
@@ -1,4 +1,5 @@
 tmp_file = ${.OBJDIR}/err.log
+replace_cmd =	sed -e 's/Bad.*$$/Bad file descriptor/'
 
 .PHONY : test_output
 test_output:
@@ -6,7 +7,7 @@ test_output:
 	${.OBJDIR}/errtest 1 2>&1; echo '$$?='$$?; \
 	${.OBJDIR}/errtest 2 2>&1; echo '$$?='$$?; \
 	${.OBJDIR}/errtest 3 2>&1; echo '$$?='$$?; \
-	${.OBJDIR}/errtest 4 2>&1 | sed 's/^main:.*$$/main: Bad file descriptor/'; \
+	${.OBJDIR}/errtest 4 2>&1 | ${replace_cmd}; \
 	${.OBJDIR}/errtest 5 2>&1; echo '$$?='$$?; \
 	${.OBJDIR}/errtest 6 2>&1; echo '$$?='$$?; \
 	ulimit -c 0; ${.OBJDIR}/errtest 7 >${.OBJDIR}/err.log 2>&1; ex=$$?; \
@@ -17,7 +18,8 @@ test_output:
 	awk 'NF > 0 {$$1="ddd"; $$2="mmm"; $$3="DD"; $$4="hh:mm:ss"; \
 		$$5="YYYY"; $$6="hostname"; \
 		sub(/errtest[^ ]+/, "errtest[nnnn]:")} \
-		{print $$0}' ${LOGFILE}; \
+		{print $$0}' ${LOGFILE} | \
+	  ${replace_cmd}; \
 	\
 	${MAKE} ${MAKEFLAGS} cleandir >/dev/null 2>&1; \
 	true
